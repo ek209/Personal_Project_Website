e885c80709ee2b45d5dfa636706e5790110d1c0c
erik kjell <freshhhhoi@gmail.com>
2023-08-16 09:12:12 -0500

    add time_processor, updated footer.html

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import os
 from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap5
+import datetime
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.getenv('PERSON_SITE_SECRET_KEY')
 Bootstrap5(app)
+
+
+@app.context_processor
+def time_processor():
+    return dict(todays_date_data=datetime.date.today())
 
 @app.route('/')
 def home():
