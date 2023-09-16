b62dd49b7102d8ed1f52f442554960b2344740a2
erik kjell <freshhhhoi@gmail.com>
2023-09-15 20:57:50 -0500

    watermark docstrings

--- watermark.py
+++ watermark.py
@@ -2,13 +2,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 from skimage import data
 
-#Finds the integer number of times number_2 goes into number_1 and adds 1
-#This is used to iterate proper num of times for watermark to be drawn
+
 def find_range(num_1, num_2):
+    """Performs Integer division, used for number of times to watermark
+    Args:
+        num_1 (Int): Dividend
+        num_2 (Int): Divisor
+
+    Returns:
+        Int: Quoitient rounded plus one to ensure coverage for watermark.
+    """
     return int(round((num_1 / num_2)) + 1)
 
 class Watermark():
     def __init__(self, font_size, font_name, text):
+        #TODO Add more fonts.
+        """Initializes watermark object
+
+        Args:
+            font_size (Int): Size of font
+            font_name (String): Name of font to use from font dict.
+            text (String): Text to watermark on image
+        """
         self.fig = plt.figure()
         self.font_dict = {'Dejavu Sans Mono' : 
                           { 'font_height' : 1,
@@ -18,15 +33,32 @@ class Watermark():
         self.text = text
     
     def set_font_size_in(self):
+        """Converts font size from pts to inches. Takes font height and width
+        from font dictionary.
+
+        Returns:
+            Float: Height in inches.
+            Float: Width in inches.
+        """
         font_size_in = self.font_size / 72
         font_height = self.font_dict[self.font_name]['font_height'] * font_size_in 
         font_width = self.font_dict[self.font_name]['font_width'] * font_size_in
         return font_height, font_width
 
     def add_space(self):
+        """Adds space to watermark text.
+        """
         self.text += " "
 
     def make_watermark(self, img):
+        """Creates a watermark on the image based on self attributes.
+
+        Args:
+            img (ndarray): Image data as ndarray
+
+        Returns:
+            ndarray: Returns watermarked image as ndarray.
+        """
         #resizes fig to img_size
         fig = self.fig
         fig.figimage(img, resize=True)
